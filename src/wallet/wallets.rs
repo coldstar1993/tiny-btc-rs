69993f08808c38e9ee292a203cb8a4e0fae37646
coldstar1993 <coldstar1993@163.com>
2024-07-07 11:12:00 +0800

    add wallets hashmap

---
+++ src/wallet/wallets.rs
@@ -0,0 +1,48 @@
+//! bitcoin wallet
+
+use crate::common::Result;
+use crate::wallet::wallet::*;
+use bincode::{deserialize, serialize};
+use bitcoincash_addr::*;
+use crypto::digest::Digest;
+use crypto::ed25519;
+use crypto::sha2::Sha256;
+use rand::{Rng, RngCore};
+use serde::{Deserialize, Serialize};
+use sled;
+use std::collections::HashMap;
+use log::info;
+use rand::rngs::OsRng;
+
+pub struct Wallets {
+    wallets: HashMap<String, Wallet>,
+}
+
+impl Wallets {
+    /// NewWallets creates Wallets and fills it from a file if it exists
+    pub fn new() -> Result<Wallets> {
+        let mut wlt = Wallets {
+            wallets: HashMap::<String, Wallet>::new(),
+        };
+        let db = sled::open("data/wallets")?;
+
+        for item in db.into_iter() {
+            let i = item?;
+            let address = String::from_utf8(i.0.to_vec())?;
+            let wallet = deserialize(&i.1.to_vec())?;
+            wlt.wallets.insert(address, wallet);
+        }
+        drop(db);
+        Ok(wlt)
+    }
+
+    /// CreateWallet adds a Wallet to Wallets
+    pub fn create_wallet(&mut self) -> String {
+        let wallet = Wallet::new();
+        let address = wallet.get_address();
+        self.wallets.insert(address.clone(), wallet);
+        info!("create wallet: {}", address);
+        address
+    }
+
+}
