8ee0876c90b6e0e88d2d81b847177684f054d3b9
coldstar1993 <coldstar1993@163.com>
2024-07-10 07:40:00 +0800

    add pow fn for block

--- src/block/block.rs
+++ src/block/block.rs
@@ -1,7 +1,14 @@
 //! Block implement of blockchain
-
+use crate::common::Result;
+use crate::{block::mergevu8::MergeVu8};
 use crate::txn::*;
+use bincode::serialize;
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+use log::info;
+use merkle_cbt::merkle_tree::CBMT;
 use serde::{Deserialize, Serialize};
+use std::time::SystemTime;
 
 const TARGET_HEXS: usize = 4;
 
@@ -15,4 +22,55 @@ pub struct Block {
     hash: String,
     nonce: i32,
     height: i32,
+}
+
+impl Block {
+    /// Run performs a proof-of-work
+    fn run_proof_of_work(&mut self) -> Result<()> {
+        info!("Mining the block");
+        while !self.validate()? {
+            self.nonce += 1;
+        }
+        let data = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data[..]);
+        self.hash = hasher.result_str();
+        Ok(())
+    }
+
+    /// HashTransactions returns a hash of the transactions in the block
+    fn hash_transactions(&self) -> Result<Vec<u8>> {
+        let mut transactions = Vec::new();
+        for tx in &self.transactions {
+            transactions.push(tx.hash()?.as_bytes().to_owned());
+        }
+
+        // construct a Merkle Complete Binary Tree (merkle_cbt)
+        let tree = CBMT::<Vec<u8>, MergeVu8>::build_merkle_tree(&transactions);
+
+        Ok(tree.root())
+    }
+
+    fn prepare_hash_data(&self) -> Result<Vec<u8>> {
+        let content = (
+            self.prev_block_hash.clone(),
+            self.hash_transactions()?,
+            self.timestamp,
+            TARGET_HEXS,
+            self.nonce,
+        );
+        let bytes = serialize(&content)?;
+        Ok(bytes)
+    }
+
+    /// Validate validates block's PoW, <br>
+    /// TODO need upgrade to Dynamic Difficulty
+    fn validate(&self) -> Result<bool> {
+        let data = self.prepare_hash_data()?;
+        let mut hasher = Sha256::new();
+        hasher.input(&data[..]);
+        let mut vec1: Vec<u8> = Vec::new();
+        vec1.resize(TARGET_HEXS, '0' as u8);
+        Ok(&hasher.result_str()[0..TARGET_HEXS] == String::from_utf8(vec1)?)
+    }
 }
