5373b9b3ee72d1b95f524905d1486fc48f258fd8
coldstar1993 <coldstar1993@163.com>
2024-07-10 08:34:00 +0800

    add new a block fn

--- src/block/block.rs
+++ src/block/block.rs
@@ -25,6 +25,27 @@ pub struct Block {
 }
 
 impl Block {
+    /// NewBlock creates and returns Block
+    pub fn new_block(
+        transactions: Vec<Transaction>,
+        prev_block_hash: String,
+        height: i32,
+    ) -> Result<Block> {
+        let timestamp = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)?
+            .as_millis();
+        let mut block = Block {
+            timestamp,
+            transactions,
+            prev_block_hash,
+            hash: String::new(),
+            nonce: 0,
+            height,
+        };
+        block.run_proof_of_work()?;
+        Ok(block)
+    }
+
     /// Run performs a proof-of-work
     fn run_proof_of_work(&mut self) -> Result<()> {
         info!("Mining the block");
