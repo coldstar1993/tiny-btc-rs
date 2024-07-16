f0fdeee759682a8e6bba8c0326a27ba50f7efedb
coldstar1993 <coldstar1993@163.com>
2024-07-16 09:46:00 +0800

    add fn create blockchain

---
+++ src/blockchain/bc.rs
@@ -0,0 +1,46 @@
+//! Blockchain
+
+use super::*;
+use crate::common::Result;
+use crate::block::{block::*};
+use crate::txn::*;
+use bincode::{deserialize, serialize};
+use failure::format_err;
+use log::{debug, info};
+use sled;
+use std::collections::HashMap;
+
+const GENESIS_COINBASE_DATA: &str =
+    "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
+
+/// Blockchain implements interactions with a DB
+#[derive(Debug)]
+pub struct Blockchain {
+    pub tip: String,
+    pub db: sled::Db,
+}
+
+
+impl Blockchain {
+
+    /// CreateBlockchain creates a new blockchain DB
+    pub fn create_blockchain(address: String) -> Result<Blockchain> {
+        info!("Creating new blockchain");
+
+        std::fs::remove_dir_all("data/blocks").ok();
+        let db = sled::open("data/blocks")?;
+        debug!("Creating new block database");
+        let cbtx = Transaction::new_coinbase(address, String::from(GENESIS_COINBASE_DATA))?;
+        let genesis: Block = Block::new_genesis_block(cbtx);
+        db.insert(genesis.get_hash(), serialize(&genesis)?)?;
+        db.insert("LAST", genesis.get_hash().as_bytes())?;
+        let bc = Blockchain {
+            tip: genesis.get_hash(),
+            db,
+        };
+        bc.db.flush()?;
+        Ok(bc)
+    }
+
+}
+
