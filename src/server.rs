3f269e8cd16563afc2a2f0c5b8ea3164a03da1e6
coldstar1993 <coldstar1993@163.com>
2024-08-06 13:16:00 +0800

    init node server for communication with other nodes, meanwhile update blockchain

---
+++ src/server.rs
@@ -0,0 +1,57 @@
+//! server of Blockchain
+
+use super::*;
+use crate::blockchain::UTXOSet;
+
+use crate::block::Block;
+use crate::txn::Transaction;
+
+use bincode::{deserialize, serialize};
+use failure::format_err;
+use log::{debug, info};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::{HashMap, HashSet};
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+use std::sync::*;
+use std::thread;
+use std::time::Duration;
+
+pub struct Server {
+    /**
+     * current node address(ip:port)
+     */
+    node_address: String,
+    /**
+     * current node mining wallet addr
+     */
+    mining_address: String,
+    /**
+     * commonly shared instance
+     */
+    inner: Arc<Mutex<ServerInner>>,
+}
+
+struct ServerInner {
+    /**
+     * neighbor nodes: will be updated dynamically by communication with other nodes
+     */
+    known_nodes: HashSet<String>,
+    /**
+     * utxo set of ledger: will be updated dynamically with new block
+     */
+    utxo: UTXOSet,
+    /**
+     * blocks need to be fetched back
+     */
+    blocks_in_transit: Vec<String>,
+    /**
+     * memory pool: storing tx of one block time range
+     */
+    mempool: HashMap<String, Transaction>,
+}
+
+const CMD_LEN: usize = 12;
+const VERSION: i32 = 1;
