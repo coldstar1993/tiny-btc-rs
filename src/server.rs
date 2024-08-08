4e8adcd68cc282274a81d593eb0121fd9cd549a6
coldstar1993 <coldstar1993@163.com>
2024-08-08 13:06:00 +0800

    add fn (de)serialize CMD of communication protocol

--- src/server.rs
+++ src/server.rs
@@ -105,3 +105,48 @@ struct ServerInner {
 const CMD_LEN: usize = 12;
 const VERSION: i32 = 1;
 
+
+fn cmd_to_bytes(cmd: &str) -> [u8; CMD_LEN] {
+    let mut data = [0; CMD_LEN];
+    for (i, d) in cmd.as_bytes().iter().enumerate() {
+        data[i] = *d;
+    }
+    data
+}
+
+fn bytes_to_cmd(bytes: &[u8]) -> Result<Message> {
+    let mut cmd = Vec::new();
+    let cmd_bytes = &bytes[..CMD_LEN];
+    let data = &bytes[CMD_LEN..];
+    for b in cmd_bytes {
+        if 0 as u8 != *b {
+            cmd.push(*b);
+        }
+    }
+    info!("cmd: {}", String::from_utf8(cmd.clone())?);
+
+    if cmd == "addr".as_bytes() {
+        let data: Vec<String> = deserialize(data)?;
+        Ok(Message::Addr(data))
+    } else if cmd == "block".as_bytes() {
+        let data: Blockmsg = deserialize(data)?;
+        Ok(Message::Block(data))
+    } else if cmd == "inv".as_bytes() {
+        let data: Invmsg = deserialize(data)?;
+        Ok(Message::Inv(data))
+    } else if cmd == "getblocks".as_bytes() {
+        let data: GetBlocksmsg = deserialize(data)?;
+        Ok(Message::GetBlock(data))
+    } else if cmd == "getdata".as_bytes() {
+        let data: GetDatamsg = deserialize(data)?;
+        Ok(Message::GetData(data))
+    } else if cmd == "tx".as_bytes() {
+        let data: Txmsg = deserialize(data)?;
+        Ok(Message::Tx(data))
+    } else if cmd == "version".as_bytes() {
+        let data: Versionmsg = deserialize(data)?;
+        Ok(Message::Version(data))
+    } else {
+        Err(format_err!("Unknown command in the server"))
+    }
+}
