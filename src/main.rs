507f36f1559dbc29db468b823b0c15c4688cc554
coldstar1993 <coldstar1993@163.com>
2024-10-12 22:02:00 +0800

    improve main

--- src/main.rs
+++ src/main.rs
@@ -23,5 +23,8 @@ pub static CONFIG: LazyLock<RwLock<AppConfig>> = LazyLock::new(||{
 fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
+    let mut cli = Cli::new()?;
+    cli.run()?;
+
     Ok(())
 }
